2b9b9e54424fe759a7f308ebc068d6846ff2a6d4
Lisa Doan <ldoan24@gmail.com>
2021-10-27 09:03:00 -0700

    Completed node101-hello-http-server

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+const server = http.createServer(function (req, res) {
+  console.log("Recieved " + req.method + " request for: " + req.url);
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello World");
+});
+
+server.listen(8080, "localhost", null, function () {
+  console.log("Server is listening on localhost: 8080");
+});
